73e2dee4096cf9123142aeff86462dadf0447fea
JoVanko <1728931104@qq.com>
2025-01-22 11:31:42 +0800

    1 提交gitignore

--- Assignment0/main.cpp
+++ Assignment0/main.cpp
@@ -6,7 +6,7 @@
 int main(){
 
     // Basic Example of cpp
-    std::cout << "Example of cpp \n";
+    std::cout << "Example of cpp\n";
     float a = 1.0, b = 2.0;
     std::cout << a << std::endl;
     std::cout << a/b << std::endl;
